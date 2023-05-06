53327243d85252a1512813bb050acead26d30459
touredri <dcompte223@gmail.com>
2023-05-06 01:58:06 +0100

    Added categories component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import BookList from './components/BookList';
+import Categories from './components/Categories';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<BookList />} />
-        <Route path="/categories" element={<h3>Coming soon</h3>} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </div>
   );

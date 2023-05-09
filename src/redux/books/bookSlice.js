240165e226bf4afcc4e30e512f63196736d7c7a2
touredri <dcompte223@gmail.com>
2023-05-09 09:49:01 +0100

    initialize

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -4,7 +4,26 @@ import { createSlice } from '@reduxjs/toolkit';
 const bookSlice = createSlice({
   name: 'books',
   initialState: {
-    books: [],
+    books: [
+      {
+        item_id: 'item1',
+        title: 'The Great Gatsby',
+        author: 'John Smith',
+        category: 'Fiction',
+      },
+      {
+        item_id: 'item2',
+        title: 'Anna Karenina',
+        author: 'Leo Tolstoy',
+        category: 'Fiction',
+      },
+      {
+        item_id: 'item3',
+        title: 'The Selfish Gene',
+        author: 'Richard Dawkins',
+        category: 'Nonfiction',
+      },
+    ],
   },
   reducers: {
     addBook: (state, action) => {

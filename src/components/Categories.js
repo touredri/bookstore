7b852376c8ac8e4206a2eef113c1adfda7e7b5ab
touredri <dcompte223@gmail.com>
2023-05-09 06:55:05 +0100

    Added button check status

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Categories() {
-  return <div>Categories coming soon</div>;
+  return (
+    <div>
+      <button type="button">Check status</button>
+    </div>
+  );
 }

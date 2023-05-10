74de68a532cf638748cc352c3660e3458172c608
touredri <dcompte223@gmail.com>
2023-05-10 11:57:05 +0000

    used useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addBook } from '../redux/books/bookSlice';
 import './book.css';
 
 export default function Form() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
   const books = useSelector((store) => store.books);
   const handleSubmit = (e) => {
@@ -18,8 +20,8 @@ export default function Form() {
     <div className="add">
       <h2>ADD A NEW BOOK</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Book title" required />
-        <input type="text" placeholder="Author" required />
+        <input type="text" placeholder="Book title" value={title} onChange={(e) => setTitle(e.target.value)} required />
+        <input type="text" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} required />
         <button type="submit"> Add </button>
       </form>
     </div>

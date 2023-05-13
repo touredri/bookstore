e61dc01e5bd1c848208c5dabe69f795fcf0a7f2c
touredri <dcompte223@gmail.com>
2023-05-13 19:36:36 +0100

    Added style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import img from './profil.png';
 import './book.css';
 
 export default function Navbar() {
   return (
-    <div className="nav">
-      <h1 className="Bookstore-CMS">Bookstore CMS</h1>
-      <div className="links">
-        <Link to="/" className="book">Books</Link>
-        <Link to="/categories">Categories</Link>
+    <div className="navBar">
+      <div className="nav">
+        <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+        <div className="links">
+          <Link to="/" className="book">
+            Books
+          </Link>
+          <Link to="/categories" className="CATEGORIES">Categories</Link>
+        </div>
       </div>
+      <img src={img} alt="profile" className="profile" />
     </div>
   );
 }
